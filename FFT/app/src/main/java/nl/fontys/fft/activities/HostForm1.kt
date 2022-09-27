0f1562eda810ee50976835ad0c529936f0482c3f
Alexandru Aciobanitei <alexaciobanitei@Alexandrus-MacBook-Pro.local>
2022-09-28 01:48:49 +0200

    last update

--- FFT/app/src/main/java/nl/fontys/fft/activities/HostForm1.kt
+++ FFT/app/src/main/java/nl/fontys/fft/activities/HostForm1.kt
@@ -1,12 +1,20 @@
 package nl.fontys.fft.activities
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.database.DatabaseReference
@@ -20,8 +28,11 @@ import java.lang.IllegalStateException
 
 class HostForm1 : AppCompatActivity() {
 
-    var num = 0;
-    
+    var num = 0
+    private val CAMERA_PERMISSION_CODE=134
+    private val STORAGE_PERMISSION_CODE=345
+    //val imgMap = findViewById<ImageView>(R.id.imgMap)
+
 
 
 
@@ -29,11 +40,14 @@ class HostForm1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host_form1)
 
+
+
         //lateinit var binding: ActivityMainBinding
         //lateinit var database: DatabaseReference
 
 
         //INTENTS FOR HOST POST
+
 
         var buttonPlus = findViewById<Button>(R.id.buttonPlus)
         var textViewNumber = findViewById<TextView>(R.id.textViewNumber)
@@ -44,8 +58,45 @@ class HostForm1 : AppCompatActivity() {
         var textDescription = findViewById<TextInputEditText>(R.id.textDescriptionField)
         var textCity = findViewById<TextInputEditText>(R.id.textCityField)
         var addImg = findViewById<Button>(R.id.addImg)
+        var galleryImg = findViewById<Button>(R.id.galleryImg)
+
+
 
         //database = FirebaseDatabase.getInstance().getReference("Ads")
+
+
+
+
+        //OPEN CAMERA/GALLERY
+
+        addImg.setOnClickListener {
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 123)
+        }
+
+        galleryImg.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 456)
+        }
+
+
+
+
+
+        //ASK PERMISSION
+
+//        addImg.setOnClickListener {
+//            checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+//        }
+//
+//       galleryImg.setOnClickListener {
+//            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+//        }
+
+
+
+
 
         btnBackHost.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -62,6 +113,7 @@ class HostForm1 : AppCompatActivity() {
             bundle1.putString("locationCity", textCity.text.toString())
             bundle1.putString("Description", textDescription.text.toString())
             intent.putExtras(bundle1)
+            intent.putExtras(intent)
             startActivity(intent)
 
             val spots = textViewNumber.text.toString()
@@ -72,25 +124,13 @@ class HostForm1 : AppCompatActivity() {
             saveFireStore(spots, country, city, description)
 
         }
-        
-        
-        //ADD IMAGE
-
-//        addImg.setOnClickListener {
-//            val intent = Intent(Intent.ACTION_PICK)
-//            intent.type = "image/*"
-//            startActivityForResult(intent, 123)
-//        }
-        
 
 
         //COUNTER
 
 
         buttonPlus.setOnClickListener{
-
             num++
-
             textViewNumber.text = num.toString()
 
 
@@ -105,6 +145,32 @@ class HostForm1 : AppCompatActivity() {
 
         }
     }
+
+
+    //val imgMap = findViewById<ImageView>(R.id.imgMap)
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+
+            var bmp : Bitmap = data?.extras?.get("data") as Bitmap
+            val imgMap = findViewById<ImageView>(R.id.imgMap)
+            imgMap.setImageBitmap(bmp)
+        }
+        else if (requestCode == 456){
+
+            val imgMap = findViewById<ImageView>(R.id.imgMap)
+            imgMap.setImageURI(data?.data)
+        }
+    }
+
+
+
+
+
+
+    //DATABASE
 
     fun saveFireStore (numberSpots: String, locationCountry: String, locationCity: String, Descritpion: String) {
         val database = FirebaseFirestore.getInstance()
@@ -122,6 +188,46 @@ class HostForm1 : AppCompatActivity() {
             .addOnFailureListener{
                 Toast.makeText(this, "Failed to create ad", Toast.LENGTH_SHORT).show()
             }
+
     }
 
+
+
+
+    //CAMERA PERMISSION AND ACCESS
+
+
+
+
+//    private fun checkPermission (permission:String, requestCode:Int) {
+//        if (ContextCompat.checkSelfPermission(this, permission)==PackageManager.PERMISSION_DENIED)
+//
+//            //ASK FOR PERMISSION CAMERA
+//
+//            ActivityCompat.requestPermissions(this, arrayOf(permission),requestCode)
+//
+//
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        if(requestCode == CAMERA_PERMISSION_CODE){
+//
+//            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+//                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        else if (requestCode == STORAGE_PERMISSION_CODE){
+//
+//            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+//                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//    }
 }
--- FFT/app/src/main/java/nl/fontys/fft/activities/HostPost.kt
+++ FFT/app/src/main/java/nl/fontys/fft/activities/HostPost.kt
@@ -1,10 +1,12 @@
 package nl.fontys.fft.activities
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import nl.fontys.fft.R
@@ -20,6 +22,7 @@ class HostPost : AppCompatActivity() {
         var tvCountry = findViewById<TextView>(R.id.tvCountry)
         var tvCity = findViewById<TextView>(R.id.tvCity)
         val tvDescription = findViewById<TextView>(R.id.tvDescription)
+        val imagePost = findViewById<ImageView>(R.id.imagePost)
 
         val bundle1 = intent.extras
 
@@ -29,29 +32,10 @@ class HostPost : AppCompatActivity() {
         tvDescription?.setText(bundle1!!.getString("Description", "info"))
 
 
-//       val numberSpots = intent.getStringExtra("numberSpots")
-//        tvSpots.text = numberSpots
-
-        //val country = intent.getStringExtra("country")
-        //tvCountry.text = country
-
-        //Toast.makeText(this, "${numberSpots}", Toast.LENGTH_LONG).show()
-
-
-
-
 
 
     }
 
 }
 
-
-
-//    btnSend.setOnClickListener {
-//
-//        string = etDescription.editText.toString()
-//        textView.text = string
-//
-//    }
 
